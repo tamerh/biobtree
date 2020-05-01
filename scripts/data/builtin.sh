12041ab09438936ee5e322f6bb898c85ba67c1b2
tamerh <tamerg01@gmail.com>
2020-05-01 21:55:11 +0100

    script for fetching ensembl files and folders

--- scripts/data/builtin.sh
+++ scripts/data/builtin.sh
@@ -56,25 +56,25 @@ prepCache(){
 # taxids 9606,10090,4932,3702,7227,6239,562,511145,83333,7955
 
 
-### CACHE 0 small demo dataset for biobtreeR ~ 36 MB db size
-./biobtree -d go,hgnc,uniprot,ensembl,interpro --uniprot.file test_data/RdemoData/uniprot_sample.xml.gz --interpro.file test_data/RdemoData/interpro_sample.xml.gz --ensembl.file test_data/RdemoData/human.chr.21.gff3.gz --go.file test_data/RdemoData/go_sample.owl -tax 9606 -idx demo update
-prepCache "demo" "36000000"
+# ### CACHE 0 small demo dataset for biobtreeR ~ 36 MB db size
+# ./biobtree -d go,hgnc,uniprot,ensembl,interpro --uniprot.file test_data/RdemoData/uniprot_sample.xml.gz --interpro.file test_data/RdemoData/interpro_sample.xml.gz --ensembl.file test_data/RdemoData/human.chr.21.gff3.gz --go.file test_data/RdemoData/go_sample.owl -tax 9606 -idx demo update
+# prepCache "demo" "36000000"
 
-### CACHE 1 datasets with above ensembl genomes except mouse strains. ~ 5.2 db size
-./biobtree --d hgnc,hmdb,uniprot,taxonomy,go,efo,eco,chebi,interpro -tax 9606,10090,4932,3702,7227,6239,562,511145,83333,7955,9031,10116 --skip-ensembl -idx builtinset1 update
-./biobtree --d ensembl --tax 9606,4932,7227,6239,7955,9031,10116 -keep --otaxids 9606,10090,4932,3702,7227,6239,562,511145,83333,7955,9031,10116 -idx builtinset12 update
-./biobtree --d ensembl_bacteria --genome "escherichia_coli,escherichia_coli_str_k_12_substr_mg1655,escherichia_coli_k_12" --keep --otaxids 9606,10090,4932,3702,7227,6239,562,511145,83333,7955,9031,10116 -idx builtinset13 update
-./biobtree --d ensembl_plants --tax 3702 --keep --otaxids 9606,10090,4932,3702,7227,6239,562,511145,83333,7955,9031,10116 -idx builtinset14 update
-./biobtree --d ensembl --genome mus_musculus -keep --otaxids 9606,10090,4932,3702,7227,6239,562,511145,83333,7955,9031,10116  -idx builtinset15 update
-prepCache "set1" "4600000000"
+# ### CACHE 1 datasets with above ensembl genomes except mouse strains. ~ 5.2 db size
+# ./biobtree --d hgnc,hmdb,uniprot,taxonomy,go,efo,eco,chebi,interpro -tax 9606,10090,4932,3702,7227,6239,562,511145,83333,7955,9031,10116 --skip-ensembl -idx builtinset1 update
+# ./biobtree --d ensembl --tax 9606,4932,7227,6239,7955,9031,10116 -keep --otaxids 9606,10090,4932,3702,7227,6239,562,511145,83333,7955,9031,10116 -idx builtinset12 update
+# ./biobtree --d ensembl_bacteria --genome "escherichia_coli,escherichia_coli_str_k_12_substr_mg1655,escherichia_coli_k_12" --keep --otaxids 9606,10090,4932,3702,7227,6239,562,511145,83333,7955,9031,10116 -idx builtinset13 update
+# ./biobtree --d ensembl_plants --tax 3702 --keep --otaxids 9606,10090,4932,3702,7227,6239,562,511145,83333,7955,9031,10116 -idx builtinset14 update
+# ./biobtree --d ensembl --genome mus_musculus -keep --otaxids 9606,10090,4932,3702,7227,6239,562,511145,83333,7955,9031,10116  -idx builtinset15 update
+# prepCache "set1" "4600000000"
 
-### CACHE 2 datasets with ensembl human and all mouse strains genomes ~ 4 db size
-./biobtree -d hgnc,hmdb,uniprot,taxonomy,go,efo,eco,chebi,interpro -tax 9606,10090 -idx builtinset2 update
-prepCache "set2" "4100000000"
+# ### CACHE 2 datasets with ensembl human and all mouse strains genomes ~ 4 db size
+# ./biobtree -d hgnc,hmdb,uniprot,taxonomy,go,efo,eco,chebi,interpro -tax 9606,10090 -idx builtinset2 update
+# prepCache "set2" "4100000000"
 
-### CACHE 3 datasets with no esembl and full uniprot ~ 3.2 db size
-./biobtree -d hgnc,hmdb,uniprot,taxonomy,go,efo,eco,chebi,interpro -idx builtinset3 update
-prepCache "set3" "3600000000"
+# ### CACHE 3 datasets with no esembl and full uniprot ~ 3.2 db size
+# ./biobtree -d hgnc,hmdb,uniprot,taxonomy,go,efo,eco,chebi,interpro -idx builtinset3 update
+# prepCache "set3" "3600000000"
 
 ### CACHE 4 datasets with no esembl and full uniprot and chembl ~ 11.5 db size
 ./biobtree -d chembl,hgnc,hmdb,uniprot,taxonomy,go,efo,eco,chebi,interpro -idx builtinset4 update
---
+++ scripts/data/ensembl_ftp.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+# Workaround script related with issue when there is a new ensembl release to build the paths
+# Output of this script used with local ftp server
+
+set -e
+
+if [[ -z $1 ]]; then
+    echo " Target directory path is requried."
+    exit 1
+fi
+
+if [[ -z $2 ]]; then
+    echo " Ensembl release number required."
+    exit 1
+fi
+
+if [[ -z $3 ]]; then
+    echo " Ensembl Genomes release number required."
+    exit 1
+fi
+
+TARGET=$1
+E_RELEASE=$2
+EG_RELEASE=$3
+
+rm -rf $TARGET
+mkdir $TARGET
+
+cd $TARGET
+
+E_FTP="ftp.ensembl.org"
+EG_FTP="ftp.ensemblgenomes.org"
+
+# try these alternative in case of issue
+#E_FTP="ftp.ebi.ac.uk/ensemblorg"
+#EG_FTP="ftp.ebi.ac.uk/ensemblgenomes"
+
+
+creat_dir_and_files () {
+    CUR_DIR=`pwd`
+    mkdir -p $1
+    cd $1    
+    while IFS= read -r line; do
+        if [[ "$line" == *: ]]; then
+            mkdir -p ${line%?} || true
+        elif [[ "$line" == *.gz ]]; then
+            touch $line
+        fi
+    done < $CUR_DIR'/'$2
+    cd $CUR_DIR
+}
+
+creat_dir_and_files_json () {
+    CUR_DIR=`pwd`
+    mkdir -p $1
+    cd $1    
+    while IFS= read -r line; do
+        if [[ "$line" == *: ]]; then
+            mkdir -p ${line%?} || true
+        elif [[ "$line" == *.json ]]; then
+            touch $line
+        fi
+    done < $CUR_DIR'/'$2
+    cd $CUR_DIR
+}
+
+creat_dir_and_files_mysql () {
+    CUR_DIR=`pwd`
+    mkdir -p $1
+    cd $1    
+    while IFS= read -r line; do
+        if [[ "$line" == *: ]]; then
+            mkdir -p ${line%?} || true
+        elif [[ "$line" == *__efg_*.gz ]]; then
+            touch $line
+        fi
+    done < $CUR_DIR'/'$2
+    cd $CUR_DIR
+}
+
+
+lftp -e 'nlist -R; quit' $E_FTP'/pub/current_json' > e_json.txt
+echo "e json done"
+creat_dir_and_files_json 'ensembl/pub/current_json' 'e_json.txt'
+lftp -e 'nlist -R; quit' $E_FTP'/pub/current_gff3' > e_gff3.txt
+echo "e gff3 done"
+creat_dir_and_files 'ensembl/pub/current_gff3' 'e_gff3.txt'
+lftp -e 'nlist -R; quit' $E_FTP'/pub/current_mysql/ensembl_mart_'$E_RELEASE > e_mysql.txt
+echo "e mysql done"
+creat_dir_and_files_mysql 'ensembl/pub/current_mysql/ensembl_mart_'$E_RELEASE 'e_mysql.txt'
+
+declare -a EG_BRANCHES=("fungi" "metazoa" "protists" "plants" "bacteria")
+for branch in "${EG_BRANCHES[@]}"
+    do
+    lftp -e 'nlist -R; quit' $EG_FTP'/pub/current/'${branch}'/json' > eg_${branch}_json.txt
+    echo "eg ${branch} json done"
+    creat_dir_and_files_json 'ensemblg/pub/current/'${branch}'/json' 'eg_'${branch}'_json.txt'
+    lftp -e 'nlist -R; quit' $EG_FTP'/pub/current/'${branch}'/gff3' > eg_${branch}_gff3.txt
+    echo "eg ${branch} gff3 done"
+    creat_dir_and_files 'ensemblg/pub/current/'${branch}'/gff3' 'eg_'${branch}'_gff3.txt'
+    if [[ "$branch" != "bacteria" ]]; then
+        lftp -e 'nlist -R; quit' $EG_FTP'/pub/current/'${branch}'/mysql/'${branch}'_mart_'$EG_RELEASE > eg_${branch}_mysql.txt
+        echo "eg ${branch} mysql done"
+        creat_dir_and_files_mysql 'ensemblg/pub/current/'${branch}'/mysql/'${branch}'_mart_'$EG_RELEASE 'eg_'${branch}'_mysql.txt'
+    fi
+    done
+
+lftp -e 'get /pub/current/species.txt; quit' $EG_FTP
+
+# lftp -e "mirror -R {local dir} {remote dir}" -u {username},{password} {host}
+
